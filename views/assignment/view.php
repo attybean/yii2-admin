d1a7740273fd9d7cec7b44d65dcb3d27caf74413
byggmann <50108257+byggmann@users.noreply.github.com>
2019-04-30 13:37:04 +0200

    Update view.php

--- views/assignment/view.php
+++ views/assignment/view.php
@@ -21,42 +21,42 @@ $this->title = Yii::t('rbac-admin', 'Assignment') . ' : ' . $userName;
 $this->params['breadcrumbs'][] = ['label' => Yii::t('rbac-admin', 'Assignments'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = $userName;
 
-AnimateAsset::register($this);
+//AnimateAsset::register($this);
 YiiAsset::register($this);
 $opts = Json::htmlEncode([
     'items' => $model->getItems(),
 ]);
 $this->registerJs("var _opts = {$opts};");
 $this->registerJs($this->render('_script.js'));
-$animateIcon = ' <i class="glyphicon glyphicon-refresh glyphicon-refresh-animate"></i>';
+$animateIconright = '<i class="fa fa-arrow-right"></i>';
+$animateIconleft = '<i class="fa fa-arrow-left"></i>';
 ?>
-<div class="assignment-index">
+<div class="assignment-index col-md-12 org_yii2admin">
     <h1><?=$this->title;?></h1>
 
     <div class="row">
-        <div class="col-sm-5">
+        <div class="col-sm-5 nopadding">
             <input class="form-control search" data-target="available"
                    placeholder="<?=Yii::t('rbac-admin', 'Search for available');?>">
-            <select multiple size="20" class="form-control list" data-target="available">
+            <select multiple size="20" class="list" data-target="available">
             </select>
         </div>
-        <div class="col-sm-1">
-            <br><br>
-            <?=Html::a('&gt;&gt;' . $animateIcon, ['assign', 'id' => (string) $model->id], [
-    'class' => 'btn btn-success btn-assign',
-    'data-target' => 'available',
-    'title' => Yii::t('rbac-admin', 'Assign'),
-]);?><br><br>
-            <?=Html::a('&lt;&lt;' . $animateIcon, ['revoke', 'id' => (string) $model->id], [
-    'class' => 'btn btn-danger btn-assign',
-    'data-target' => 'assigned',
-    'title' => Yii::t('rbac-admin', 'Remove'),
-]);?>
+        <div class="col-sm-2 org_yii2admin_leftrightbuttons">
+            <?=Html::a($animateIconright, ['assign', 'id' => (string) $model->id], [
+                'class' => 'btn btn-primary btn-assign',
+                'data-target' => 'available',
+                'title' => Yii::t('rbac-admin', 'Assign'),
+            ]);?>
+            <?=Html::a($animateIconleft, ['revoke', 'id' => (string) $model->id], [
+                'class' => 'btn btn-danger btn-assign',
+                'data-target' => 'assigned',
+                'title' => Yii::t('rbac-admin', 'Remove'),
+            ]);?>
         </div>
-        <div class="col-sm-5">
+        <div class="col-sm-5 nopadding">
             <input class="form-control search" data-target="assigned"
                    placeholder="<?=Yii::t('rbac-admin', 'Search for assigned');?>">
-            <select multiple size="20" class="form-control list" data-target="assigned">
+            <select multiple size="20" class="list" data-target="assigned">
             </select>
         </div>
     </div>
