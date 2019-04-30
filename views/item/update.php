cf66a92bc99435767ce2c5238e068a4b2ddb74aa
byggmann <50108257+byggmann@users.noreply.github.com>
2019-04-30 13:39:44 +0200

    Update update.php

--- views/item/update.php
+++ views/item/update.php
@@ -10,10 +10,9 @@ $context = $this->context;
 $labels = $context->labels();
 $this->title = Yii::t('rbac-admin', 'Update ' . $labels['Item']) . ': ' . $model->name;
 $this->params['breadcrumbs'][] = ['label' => Yii::t('rbac-admin', $labels['Items']), 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['view', 'id' => $model->name]];
-$this->params['breadcrumbs'][] = Yii::t('rbac-admin', 'Update');
+$this->params['breadcrumbs'][] = Yii::t('rbac-admin', 'Update ').$model->name;
 ?>
-<div class="auth-item-update">
+<div class="auth-item-update col-md-12">
     <h1><?= Html::encode($this->title) ?></h1>
     <?=
     $this->render('_form', [
