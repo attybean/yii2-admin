3cf1f145b50954e1f7a1acdcc132cc3824892138
byggmann <50108257+byggmann@users.noreply.github.com>
2019-04-30 13:36:09 +0200

    Update index.php

--- views/assignment/index.php
+++ views/assignment/index.php
@@ -15,19 +15,46 @@ $this->params['breadcrumbs'][] = $this->title;
 
 $columns = [
     ['class' => 'yii\grid\SerialColumn'],
+    [
+        'label' => 'Id',
+        'class' => 'yii\grid\DataColumn', // can be omitted, as it is the default
+        'value' => function ($data) {
+            return $data->id; // $data['name'] for array data, e.g. using SqlDataProvider.
+        },
+    ],
     $usernameField,
 ];
 if (!empty($extraColumns)) {
     $columns = array_merge($columns, $extraColumns);
 }
+
 $columns[] = [
     'class' => 'yii\grid\ActionColumn',
-    'template' => '{view}'
+    'template' => '{view}',
+    'buttons' =>
+        [
+            'update'=>function($url,$model,$key)
+            {
+                return Html::a("<i class='fa fa-edit'></i>", $url);
+            },
+            'view'=>function($url,$model,$key)
+            {
+                return Html::a("<i class='fa fa-eye'></i>", $url);
+            },
+            'delete'=>function($url,$model,$key)
+            {
+                return Html::a("<i class='fa fa-trash-alt'></i>", $url);
+            }
+        ],
 ];
+
 ?>
-<div class="assignment-index">
+<div class="assignment-index col-md-12">
 
     <h1><?= Html::encode($this->title) ?></h1>
+
+    <?= Html::a("<i class='fa fa-user-shield'></i>".Yii::t('rbac-admin', 'Role'), "/yii2admin/role", ['class' => 'btn btn-primary']) ?>
+    <?= Html::a("<i class='fa fa-user-shield'></i>".Yii::t('rbac-admin', 'Permissons'), "/yii2admin/permission", ['class' => 'btn btn-primary']) ?>
 
     <?php Pjax::begin(); ?>
     <?=
